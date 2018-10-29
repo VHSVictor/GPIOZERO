0d733caa2375b35abaceb69651f3ca741725228c
Victor Hugo Gabriel <victor.dase@hhotmail.com>
2018-10-29 12:15:43 -0300

    add public_html

---
+++ public_html/OpenCloseDoor.py
@@ -0,0 +1,12 @@
+#!/usr/bin python
+
+import sys, json
+
+result = {'success':'true','message':'The Command Completed Successfully'};
+
+myjson = json.load(sys.stdin)
+# Do something with 'myjson' object
+print("jason:"+myjson)
+print('Content-Type: application/json\n\n')
+print(json.dumps(result))    # or "json.dump(result, sys.stdout)"
+
